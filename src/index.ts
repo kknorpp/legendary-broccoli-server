db887ce264babb0821a15318a2397f32d28a78c1
Kyle Knorpp <Kyle@Cobra-2.local>
2020-05-21 20:36:57 -0400

    added a quizes handler

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,36 @@ const PORT = process.env.PORT || 5000;
 // client.connect();
 
 const commandsApp: express.Application = express();
-commandsApp.use(cors({origin: true}));
+commandsApp.use(cors({ origin: true }));
 commandsApp.use(bodyParser.json());
-commandsApp.use(bodyParser.urlencoded({extended: true}));
+commandsApp.use(bodyParser.urlencoded({ extended: true }));
 
 commandsApp.get("/", (request: express.Request, response: express.Response) => {
-    response.send({status: "up"});
+    response.send({ status: "up" });
+});
+commandsApp.get("/quizzes", (request: express.Request, response: express.Response) => {
+    response.send({
+        quizzes: [
+            {
+                name: " ", questions: []
+            },
+            {
+                name: " ", questions: []
+            },
+            {
+                name: " ", questions: []
+            },
+            {
+                name: " ", questions: []
+            },
+            {
+                name: " ", questions: []
+            },
+            {
+                name: " ", questions: []
+            }
+        ]
+    });
 });
 
 commandsApp.listen(PORT, () => console.log(`listening on port ${PORT}`));
