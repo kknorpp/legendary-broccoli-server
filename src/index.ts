ad7596592420896ab68c0bf9657a18834f093d40
theochupp <tclchiam@gmail.com>
2020-05-21 20:12:24 -0400

    fix: disable psql stuff for now

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import * as express from "express";
 import * as cors from "cors";
 import * as bodyParser from "body-parser";
-import {Client} from "pg";
+// import {Client} from "pg";
 // import {Agent} from "https";
 // import * as rp from "request-promise";
-import {dbOptions} from "./config";
+// import {dbOptions} from "./config";
 
 const PORT = process.env.PORT || 5000;
 
 // const agent = new Agent({keepAlive: true});
 // const rpApi = rp.defaults({agent});
 
-const client = new Client(dbOptions);
-client.connect();
+// const client = new Client(dbOptions);
+// client.connect();
 
 const commandsApp: express.Application = express();
 commandsApp.use(cors({origin: true}));
